d86bde8a6caf2ccb71935d7bc5ac5a8574a66adb
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-06-09 02:14:03 +0530

    update file

--- 036_set/04_removing_ele.py
+++ 036_set/04_removing_ele.py
@@ -1,16 +1,17 @@
 #04_removing_ele
 
-
+'''
 s = {10,20,30,40,50}
 s.remove(20)
 print(s)  # {50, 40, 10, 30}
+'''
 
 '''
 s.remove(22)
 print(s) #KeyError: 22
 '''
 
-s.discard(22)
+#s.discard(22)
 
 
 #pop
@@ -27,7 +28,16 @@ s.pop()
 print(s)
 '''
 
+'''
 s = {10, 20, 30, 40}
 print(s)   #{40, 10, 20, 30}
 del s
 print(s)  #NameError: name 's' is not defined
+'''
+#clear():  it remove all the elements
+
+s = {1,2,3,4}
+print(s)            #{1, 2, 3, 4}
+s.clear()
+print(s)     #set()
+
